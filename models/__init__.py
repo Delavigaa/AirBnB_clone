32f2c9664e2417e6448c0e8467e2c12d84e8818d
Ahmed elqanwaty <ahemedf2312@gmail.com>
2024-01-16 12:39:47 -0800

    Finish

--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-import models.engine.file_storage
-
-storage = models.engine.file_storage.FileStorage()
-storage.reload()
--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
 
+
 class Amenity(BaseModel):
-	"""
-	Represents an amenity.
-	"""
-	name = ""
+    """
+    Represents an amenity.
+    """
+    name = ""
