945eb473f15954698ff71856df3f7415e49f4b7f
Ahmed Sheimy <ahmedemad456478@gmail.com>
2024-01-16 12:23:55 -0800

    done

--- models/state.py
+++ models/state.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
 
-class State(BaseModel):
-	"""
-	Represents a state.
-	"""
 
-	name = ""
+class State(BaseModel):
+    """
+    Represents a state.
+    """
+
+    name = ""
